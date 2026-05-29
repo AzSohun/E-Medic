475b7294e59833b21c4353bd1f89e6633aa79cfb
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-30 02:56:08 +0600

    Add: Medical Record Model

---
+++ E-Medic/Models/MedicalRecord.cs
@@ -0,0 +1,15 @@
+﻿namespace E_Medic.Models
+{
+    public class MedicalRecord: BaseEntity
+    {
+        public Guid PatientId { get; set; }
+        public User Patient { get; set; } = null!;
+
+        public string DiseaseName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string AttachmentFilePath { get; set; } = string.Empty;
+        public DateTime RecordDate { get; set; } = DateTime.UtcNow;
+
+        public
+    }
+}
