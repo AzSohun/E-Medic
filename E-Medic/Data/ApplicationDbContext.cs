2a73c2911b94b2b26e19823bc0d1b02249f1919d
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-30 03:29:04 +0600

    Add: ApplictionDbContext

---
+++ E-Medic/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using E_Medic.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Medic.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<MedicalRecord> MedicalRecords { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var foriegnKey in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                foriegnKey.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+    }
+}
