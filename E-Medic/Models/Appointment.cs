1f3ecc0d159f3152d0b61e83e1957627f135c578
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-30 03:05:16 +0600

    Add: Appointment Model

---
+++ E-Medic/Models/Appointment.cs
@@ -0,0 +1,18 @@
+﻿namespace E_Medic.Models
+{
+    public class Appointment: BaseEntity
+    {
+        public Guid PatientId { get; set; }
+        public User Patient { get; set; } = null!;
+
+        public Guid DoctorId { get; set; }
+        public Doctor Doctor { get; set; } = null!;
+
+        public DateTime AppointmentDate { get; set; }
+        public string Status { get; set; } = "Pending";
+        public string ProblemDescription { get; set; } = string.Empty;
+
+        public string DoctorNote { get; set; } = string.Empty;
+        public string PrescriptionFilePath { get; set; } = string.Empty;
+    }
+}
