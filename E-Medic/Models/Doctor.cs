d3ce28d94c7f4887c474c4b4bfa20deb3ed47f4e
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-30 02:45:48 +0600

    Doctor and User Model Added

---
+++ E-Medic/Models/Doctor.cs
@@ -0,0 +1,20 @@
+﻿namespace E_Medic.Models
+{
+    public class Doctor: BaseEntity
+    {
+
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+
+
+        public string Qualifiations { get; set; } = string.Empty;
+        public string Specification { get; set; } = string.Empty;
+        public string AvailableHours { get; set; } = string.Empty;
+        public bool IsApprovedByAdmin { get; set; } = false;
+
+        public string DoctorProfilePicture { get; set; } = string.Empty;
+        public decimal AverageRating { get; set; } = 0.0m;
+        public int TotalRatingCount { get; set; } = 0;
+
+    }
+}
---
+++ E-Medic/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace E_Medic.Models
+{
+    public class User: BaseEntity
+    {
+
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Role { get; set; } = "Patient";
+        public bool IsEmailVerified { get; set; } = false;
+
+        public DateTime? DateOfBirth { get; set; }
+        public string ProfilePicture { get; set; } = "profile-photo.png";
+        public string Gender { get; set; } = "Male";
+
+    }
+}
