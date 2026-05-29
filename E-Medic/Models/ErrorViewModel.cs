6a4c9a4aaff04ab1e55abee1c38b3363dfd869a3
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-29 23:19:12 +0600

    Project Initialized

---
+++ E-Medic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_Medic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
