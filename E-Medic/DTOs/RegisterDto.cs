a67182fe247f44363f20aa5c56b79c1346bef0f7
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-30 14:08:47 +0600

    Add: RegisterDto

---
+++ E-Medic/DTOs/RegisterDto.cs
@@ -0,0 +1,16 @@
+﻿namespace E_Medic.DTOs
+{
+    public class RegisterDto
+    {
+
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty;
+        public DateTime? DateOfBirth { get; set; }
+        public string Role { get; set; } = "Patient";
+
+    }
+}
