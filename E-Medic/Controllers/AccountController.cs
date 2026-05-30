05dd4a3fd90a6ee54c782b43143ed55fe7c00d30
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-30 16:09:22 +0600

    Add: Account Controller

---
+++ E-Medic/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using E_Medic.DTOs;
+using E_Medic.Services;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Medic.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly AccountService _accountService;
+        private readonly IValidator<RegisterDto> _validator;
+
+
+        public AccountController(AccountService accountService, IValidator<RegisterDto> validator)
+        {
+            _accountService = accountService;
+            _validator = validator;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Register() 
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterDto model)
+        {
+
+            var validationResult = await _validator.ValidateAsync(model);
+
+            if (!validationResult.IsValid)
+            {
+                foreach(var error in validationResult.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+
+                return View();
+
+            }
+
+
+            var result = await _accountService.RegisterUserAsync(model);
+
+            if (result.Succeeded)
+            {
+                TempData["SuccessMessage"] = "User Registration Successfull. Please Login";
+                return RedirectToAction("Login");
+            }
+
+            foreach(var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View(model);
+        }
+    }
+}
