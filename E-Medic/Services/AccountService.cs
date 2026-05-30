0b1e46a09cfd5b362a3bdcbef7618c1c72b40fd3
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-30 15:39:36 +0600

    Add: AccountService

---
+++ E-Medic/Services/AccountService.cs
@@ -0,0 +1,45 @@
+﻿using E_Medic.DTOs;
+using E_Medic.Models;
+using E_Medic.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace E_Medic.Services
+{
+    public class AccountService: IAccountService
+    {
+
+        private readonly UserManager<User> _userManager;
+
+        public AccountService(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        public async Task<IdentityResult> RegisterUserAsync(RegisterDto registerDto)
+        {
+
+            var user = new User
+            {
+                FullName = registerDto.FullName,
+                Email = registerDto.Email,
+                PhoneNumber = registerDto.PhoneNumber,
+                PasswordHash = registerDto.Password,
+                Gender = registerDto.Gender,
+                DateOfBirth = registerDto.DateOfBirth,
+                Role = registerDto.Role,
+                IsEmailVerified = false
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, registerDto.Role);
+            }
+
+            return result;
+
+        }
+    }
+}
