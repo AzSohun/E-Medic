a6b14208815ed3f9bacf86bafc83cfa796d05331
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-30 12:58:29 +0600

    Update: ApplicationDbContext

--- E-Medic/Data/ApplicationDbContext.cs
+++ E-Medic/Data/ApplicationDbContext.cs
@@ -1,11 +1,12 @@
 ﻿using E_Medic.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace E_Medic.Data
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext: IdentityDbContext<User, IdentityRole<Guid>, Guid>
     {
-
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
         {
             
