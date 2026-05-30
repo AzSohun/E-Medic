8aa84191b41d5c0a3c7b9812639efec5d9179da5
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-30 13:16:10 +0600

    Add: Identity Configuration and Cookie Configuration

--- E-Medic/Program.cs
+++ E-Medic/Program.cs
@@ -1,4 +1,6 @@
 using E_Medic.Data;
+using E_Medic.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +14,28 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// Identity
+builder.Services.AddIdentity<User, IdentityRole<Guid>>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 8;
+    options.SignIn.RequireConfirmedEmail = true; // Sign in policy for Mandatory E-mail Verification
 
+}).AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders(); // Generate Token during E-mail Verification
+
+
+// Cookie
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "Account/Login";
+    options.LogoutPath = "Account/Logout";
+    options.AccessDeniedPath = "Account/AccessDenied";
+    options.ExpireTimeSpan = TimeSpan.FromDays(7);
+    options.SlidingExpiration = true;
+});
 
 
 var app = builder.Build();
