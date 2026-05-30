c610dd634a202663ba35fa99cfd56dd6456ed47d
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-30 20:28:50 +0600

    Add: EmailService

---
+++ E-Medic/Services/EmailService.cs
@@ -0,0 +1,22 @@
+﻿using E_Medic.Services.Interfaces;
+
+namespace E_Medic.Services
+{
+    public class EmailService: IEmailService
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public async Task EmailSenderAsync(string emailTo, string subject, string htmlMessage)
+        {
+
+        }
+
+    }
+}
