68205d9fb684681b68855ef4b9d0a8b66533e417
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 00:55:37 +0600

    Add: Login and Logout Controller Added

--- E-Medic/Controllers/AccountController.cs
+++ E-Medic/Controllers/AccountController.cs
@@ -65,5 +65,29 @@ namespace E_Medic.Controllers
 
         [HttpGet]
         public IActionResult Login() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginDto model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var result = await _accountService.LoginUserAsync(model);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _accountService.LogoutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
