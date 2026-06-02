1f9fe0f7938bdb7ff4cd75e29b28c513d0811acb
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 01:18:25 +0600

    Add: Dashboard Controller

---
+++ E-Medic/Controllers/DashboardController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Medic.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User.IsInRole("Admin"))
+            {
+                return RedirectToAction("AdminDashboard");
+            }
+            else if (User.IsInRole("Doctor"))
+            {
+                return RedirectToAction("DoctorDashboard");
+            }
+            else
+            {
+                return RedirectToAction("PatientDashboard");
+            }
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Doctor")]
+        public IActionResult DoctorDashboard()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Patient")]
+        public IActionResult PatientDashboard()
+        {
+            return View();
+        }
+    }
+}
