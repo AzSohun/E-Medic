36be2edb182a25d5c3ebd2594776b5414074d7a8
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 18:28:51 +0600

    Add: Get Pending Doctor Async

--- E-Medic/Services/AdminService.cs
+++ E-Medic/Services/AdminService.cs
@@ -1,6 +1,24 @@
-﻿namespace E_Medic.Services
+﻿using E_Medic.Models;
+using E_Medic.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Medic.Services
 {
-    public class AdminService
+    public class AdminService: IAdminService
     {
+        private readonly UserManager<User> _userManager;
+
+        public AdminService(UserManager<User> _userManager)
+        {
+            this._userManager = _userManager;
+        }
+
+        public async Task<IEnumerable<User>> GetPendingDoctorsAsync()
+        {
+            return await _userManager.Users
+                .Where(u => u.Role == "Doctor" && !u.IsApprovedByAdmin)
+                .ToListAsync();
+        }
     }
 }
