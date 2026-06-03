a43c86b03fef78f4b8456310273f889dc20ed746
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 12:34:26 +0600

    Update: Customized Table Naming"

--- E-Medic/Data/ApplicationDbContext.cs
+++ E-Medic/Data/ApplicationDbContext.cs
@@ -21,6 +21,14 @@ namespace E_Medic.Data
         {
             base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<IdentityRole<Guid>>().ToTable("Roles");
+            modelBuilder.Entity<IdentityUserRole<Guid>>().ToTable("UserRoles");
+            modelBuilder.Entity<IdentityUserClaim<Guid>>().ToTable("UserClaims");
+            modelBuilder.Entity<IdentityUserLogin<Guid>>().ToTable("UserLogins");
+            modelBuilder.Entity<IdentityRoleClaim<Guid>>().ToTable("RoleClaims");
+            modelBuilder.Entity<IdentityUserToken<Guid>>().ToTable("UserTokens");
+
             foreach (var foriegnKey in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
             {
                 foriegnKey.DeleteBehavior = DeleteBehavior.Restrict;
