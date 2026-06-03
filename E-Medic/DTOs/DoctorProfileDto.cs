7cfa08b5c6994a367422630afb2d01e337e939cc
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 21:33:27 +0600

    Add: Doctor Profile Dto

--- E-Medic/DTOs/DoctorProfileDto.cs
+++ E-Medic/DTOs/DoctorProfileDto.cs
@@ -2,5 +2,10 @@
 {
     public class DoctorProfileDto
     {
+        public string Qualifications { get; set; } = string.Empty;
+        public string Specialty { get; set; } = string.Empty;
+        public string AvailableHours { get; set; } = string.Empty;
+        public decimal ConsultationFee { get; set; }
+        public int ExperienceYears { get; set; }
     }
 }
