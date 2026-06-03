468b0456cdfaa11c98e4b1d0b0bb30859fd1163d
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 22:27:05 +0600

    Migration Update

---
+++ E-Medic/Migrations/20260603162234_AddNewDoctorColumnsToDb.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_Medic.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewDoctorColumnsToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ConsultationFee",
+                table: "Doctors",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ExperienceYears",
+                table: "Doctors",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsProfileCompleted",
+                table: "Doctors",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConsultationFee",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "ExperienceYears",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "IsProfileCompleted",
+                table: "Doctors");
+        }
+    }
+}
