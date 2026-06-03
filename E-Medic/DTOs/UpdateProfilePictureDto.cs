3acfe9c54ff06cf895a867f1ca81323795d77a77
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 14:12:10 +0600

    Add: Update Profile Picture Dto

---
+++ E-Medic/DTOs/UpdateProfilePictureDto.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Medic.DTOs
+{
+    public class UpdateProfilePictureDto
+    {
+        public Guid DoctorId { get; set; }
+        public IFormFile ProfileImage { get; set; } = null!;
+    }
+}
