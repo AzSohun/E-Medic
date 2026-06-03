cfe91303b7a10a963a971a4090935825a95990d4
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 16:19:55 +0600

    Update: Register Dto

--- E-Medic/DTOs/RegisterDto.cs
+++ E-Medic/DTOs/RegisterDto.cs
@@ -2,15 +2,13 @@
 {
     public class RegisterDto
     {
-
         public string FullName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; }
         public string Password { get; set; } = string.Empty;
         public string ConfirmPassword { get; set; } = string.Empty;
-        public string Gender { get; set; } = string.Empty;
+        public string? Gender { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public string Role { get; set; } = "Patient";
-
     }
 }
