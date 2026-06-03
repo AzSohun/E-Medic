889bf6bf26e70522eb1d4097dc5a901268e53682
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 21:39:13 +0600

    Add: IDoctor Service Dto

---
+++ E-Medic/Services/Interfaces/IDoctorService.cs
@@ -0,0 +1,12 @@
+﻿using E_Medic.DTOs;
+using E_Medic.Models;
+
+namespace E_Medic.Services.Interfaces
+{
+    public interface IDoctorService
+    {
+        Task<Doctor?> GetProfileByUserIdAsync(Guid userId);
+
+        Task<bool> CompleteProfileAsync(Guid userId, DoctorProfileDto dto);
+    }
+}
