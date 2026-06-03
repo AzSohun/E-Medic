a517e4f3e0c7169f804380d72a2744bee481cb93
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 14:05:20 +0600

    Update: Medical Record Model Updated

--- E-Medic/Models/MedicalRecord.cs
+++ E-Medic/Models/MedicalRecord.cs
@@ -2,8 +2,14 @@
 {
     public class MedicalRecord: BaseEntity
     {
+        public Guid AppointmentId { get; set; }
+        public Appointment Appointment { get; set; } = null!;
+
         public Guid PatientId { get; set; }
         public User Patient { get; set; } = null!;
+
+        public Guid DoctorId { get; set; }
+        public Doctor Doctor { get; set; } = null!;
 
         public string DiseaseName { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
