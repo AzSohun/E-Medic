91aa5b34b7cd5d67b7602dfc6376ad181803bb24
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 13:42:07 +0600

    Add: Create Prescription Dto

---
+++ E-Medic/DTOs/CreatePrescriptionDto.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Medic.DTOs
+{
+    public class CreatePrescriptionDto
+    {
+        public Guid AppointmentId { get; set; }
+        public Guid PatientId { get; set; }
+        public string Diagnosis { get; set; } = string.Empty;
+        public string Medications { get; set; } = string.Empty;
+        public string DoctorNotes { get; set; } = string.Empty;
+    }
+}
