4ab35b686b991d0e32cd70ba40a596a2dae29b6a
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 14:16:42 +0600

    Update: IProescription Service interface updated

--- E-Medic/Services/Interfaces/IPrescriptionService.cs
+++ E-Medic/Services/Interfaces/IPrescriptionService.cs
@@ -1,6 +1,12 @@
-﻿namespace E_Medic.Services.Interfaces
+﻿using E_Medic.DTOs;
+using E_Medic.Models;
+
+namespace E_Medic.Services.Interfaces
 {
     public interface IPrescriptionService
     {
+        
+        Task<bool> CreatePrescriptionAsync(CreatePrescriptionDto dto);
+        Task<IEnumerable<MedicalRecord>> GetPatientHistoryAsync(Guid patientUserId);
     }
 }
