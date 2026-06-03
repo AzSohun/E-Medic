f2b92d37518303940aac78b3b9e3c61f77c36bef
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 15:39:25 +0600

    Add: Pdf Genertor Service

---
+++ E-Medic/Services/PdfService.cs
@@ -0,0 +1,46 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+using E_Medic.Services.Interfaces;
+
+namespace E_Medic.Services
+{
+    public class PdfService: IPdfService
+    {
+        private readonly IConverter _converter;
+
+        public PdfService(IConverter converter)
+        {
+            _converter = converter;
+        }
+
+        public byte[] GeneratePdfFromHtml(string htmlContent)
+        {
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10, Bottom = 10, Left = 10, Right = 10 },
+                DocumentTitle = "Medical Prescription"
+            };
+
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = htmlContent,
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = null },
+                HeaderSettings = { FontName = "Inter", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+                FooterSettings = { FontName = "Inter", FontSize = 9, Center = "E-Medic Digital Health Network", Line = true }
+            };
+
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+
+            return _converter.Convert(pdf);
+        }
+    }
+}
+}
