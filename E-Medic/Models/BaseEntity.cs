3a3dc63255ef88e9c1ea1d41ab9943d5c4d5f236
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 13:44:44 +0600

    Update: Revert BaseEntity Update

--- E-Medic/Models/BaseEntity.cs
+++ E-Medic/Models/BaseEntity.cs
@@ -5,8 +5,5 @@
         public Guid Id { get; set; } = Guid.CreateVersion7();
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? UpdatedAt { get; set; }
-        public string Diagnosis { get; set; } = string.Empty;
-        public string Medications { get; set; } = string.Empty;
-        public string DoctorNotes { get; set; } = string.Empty;
     }
 }
