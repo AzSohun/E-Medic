b86d8688bc85c1e5128119013461c5c7f9ddcdb5
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 12:32:35 +0600

    Add: Appointment Dto and Services

---
+++ E-Medic/DTOs/BookAppointmentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace E_Medic.DTOs
+{
+    public class BookAppointmentDto
+    {
+        public Guid DoctorId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public string SymptomDescription { get; set; } = string.Empty;
+    }
+}
---
+++ E-Medic/Services/Interfaces/IAppointmentService.cs
@@ -0,0 +1,13 @@
+﻿using E_Medic.DTOs;
+using E_Medic.Models;
+
+namespace E_Medic.Services.Interfaces
+{
+    public interface IAppointmentService
+    {
+        Task<IEnumerable<Doctor>> GetAllDoctorsAsync();
+        Task<bool> BookAppointmentAsync(Guid patientId, BookAppointmentDto dto);
+        Task<IEnumerable<Appointment>> GetDoctorQueueAsync(Guid doctorUserId);
+        Task<bool> UpdateAppointmentStatusAsync(Guid appointmentId, string status);
+    }
+}
