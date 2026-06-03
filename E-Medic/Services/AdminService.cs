3392df0f0ea16e4b43a5ac44e7b7cb0e198433ea
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-03 18:26:35 +0600

    Add: IAdmin Service

---
+++ E-Medic/Services/AdminService.cs
@@ -0,0 +1,6 @@
+﻿namespace E_Medic.Services
+{
+    public class AdminService
+    {
+    }
+}
---
+++ E-Medic/Services/Interfaces/IAdminService.cs
@@ -0,0 +1,11 @@
+﻿using E_Medic.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace E_Medic.Services.Interfaces
+{
+    public interface IAdminService
+    {
+        Task<IEnumerable<User>> GetPendingDoctorsAsync();
+        Task<IdentityResult> ApproveDoctorAsync(Guid doctorId);
+    }
+}
