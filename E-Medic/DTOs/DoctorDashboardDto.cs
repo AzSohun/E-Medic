ef8bb2180d14ca71119a4fad75f1421d9207f16f
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-04 14:46:56 +0600

    Add: DoctorDashboard Dto

---
+++ E-Medic/DTOs/DoctorDashboardDto.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Medic.DTOs
+{
+    public class DoctorDashboardDto
+    {
+        public int TodaysAppointmentsCount { get; set; }
+        public int TotalAppointmentsCount { get; set; }
+        public decimal TotalEarnings { get; set; }
+        public IEnumerable<AppointmentDto> TodaysQueue { get; set; } = new List<AppointmentDto>();
+    }
+}
+}
