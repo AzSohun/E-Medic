bfa7a605cf13d621faec0c2675fa6861888f8cfe
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-04 14:48:02 +0600

    Add: Appointment Dto

---
+++ E-Medic/DTOs/AppointmentDto.cs
@@ -0,0 +1,13 @@
+﻿namespace E_Medic.DTOs
+{
+    public class AppointmentDto
+    {
+        public Guid AppointmentId { get; set; }
+        public string PatientName { get; set; } = string.Empty;
+        public int PatientAge { get; set; }
+        public string PatientGender { get; set; } = string.Empty;
+        public DateTime AppointmentDate { get; set; }
+        public string Status { get; set; } = string.Empty; // e.g., Pending, Completed
+        public int SerialNumber { get; set; }
+    }
+}
