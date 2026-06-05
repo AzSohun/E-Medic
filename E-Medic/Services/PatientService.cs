609d67da1e937089315916df604a38f26e6a8d04
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-05 22:15:28 +0600

    Add: PatientService

---
+++ E-Medic/Services/PatientService.cs
@@ -0,0 +1,35 @@
+﻿using E_Medic.Data;
+using E_Medic.Models;
+using E_Medic.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Medic.Services
+{
+    public class PatientService: IPatientService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PatientService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetProfileByIdAsync(Guid userId)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+        }
+
+        public async Task<bool> UpdateProfileAsync(Guid userId, User model)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+            if (user == null) return false;
+
+            user.FullName = model.FullName;
+            user.Gender = model.Gender;
+            user.DateOfBirth = model.DateOfBirth;
+
+            _context.Users.Update(user);
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
