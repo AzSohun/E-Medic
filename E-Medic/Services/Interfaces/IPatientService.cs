3f8ee537857f88482222df3a9decb8f1bd9397b8
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-06-05 22:13:23 +0600

    Add: IPatientService Interface

---
+++ E-Medic/Services/Interfaces/IPatientService.cs
@@ -0,0 +1,10 @@
+﻿using E_Medic.Models;
+
+namespace E_Medic.Services.Interfaces
+{
+    public interface IPatientService
+    {
+        Task<User?> GetProfileByIdAsync(Guid userId);
+        Task<bool> UpdateProfileAsync(Guid userId, User model);
+    }
+}
